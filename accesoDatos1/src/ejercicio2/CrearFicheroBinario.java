9fc996aa592a77e5db6396fb565203ecec9f85cb
albertopaez <albertopaez92@gmail.com>
2019-09-26 06:41:07 +0200

    update

--- accesoDatos1/src/ejercicio2/CrearFicheroBinario.java
+++ accesoDatos1/src/ejercicio2/CrearFicheroBinario.java
@@ -1,5 +1,28 @@
 package ejercicio2;
 
+import java.io.*;
+
 public class CrearFicheroBinario {
+
+	public static void main(String[] args) {
+		
+		Departamento dep = new Departamento();
+		dep.setNumeroDepartamento(34);
+		dep.setLocalidad("Aljarafe");
+		dep.setNombre("Contabilidad");
+		
+		try {
+	         FileOutputStream fileOut =
+	         new FileOutputStream("Departamentos.dat");
+	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	         out.writeObject(dep);
+	         out.close();
+	         fileOut.close();
+	         System.out.printf("Serialized data is saved in Departamentos.dat");
+	      } catch (IOException i) {
+	         i.printStackTrace();
+	      }
+
+	}
 
 }
--- accesoDatos1/src/ejercicio2/Departamento.java
+++ accesoDatos1/src/ejercicio2/Departamento.java
@@ -1,5 +1,56 @@
 package ejercicio2;
 
-public class Departamento {
+import java.io.Serializable;
 
+public class Departamento implements Serializable {
+	int numeroDepartamento;
+	String nombre;
+	String localidad;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getNumeroDepartamento() {
+		return numeroDepartamento;
+	}
+	public void setNumeroDepartamento(int numeroDepartamento) {
+		this.numeroDepartamento = numeroDepartamento;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	
 }
